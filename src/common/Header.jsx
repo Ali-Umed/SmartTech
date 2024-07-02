22686413b49e63cb1405adaafeb8195dd1321264
Ali-Umed <aliumed844@gmail.com>
2024-07-02 23:59:57 +0300

    feat: Add navbar and header components with styles

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,3 +1,76 @@
+import { Link } from "react-router-dom";
+import "../../src/index.css";
+import { navbar } from "../data/data";
+import { useEffect, useState } from "react";
+import {
+  HiOutlineHeart,
+  HiOutlineShoppingBag,
+  HiOutlineUser,
+} from "react-icons/hi";
+import Sidebar from "./Sidebar";
+
 export default function Header() {
-  return <div>Header</div>;
+  const [sticky, setSticky] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setSticky(window.scrollY > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div>
+      <div className=" sticky header py-4 top-0 z-50 shadow-xl">
+        <div className="w-10/12 m-auto flex flex-wrap justify-between items-center">
+          <div>
+            <div className="logo">logo</div>
+          </div>
+          <div className="md:flex flex-wrap text-base py-3">
+            {navbar.map((val, key) => (
+              <div key={key} className="mr-5">
+                <Link
+                  className="active link-hover transition-all"
+                  to={val.path}
+                >
+                  {val.nav}
+                </Link>
+              </div>
+            ))}
+          </div>
+          <li className="flex flex-wrap">
+            <Link className="mr-5 text-2xl">
+              <HiOutlineHeart />
+            </Link>
+          </li>{" "}
+          <li className="flex flex-wrap">
+            <Link className="mr-5 text-2xl">
+              <HiOutlineUser />
+            </Link>
+          </li>{" "}
+          <li className="flex flex-wrap" onClick={toggleSidebar}>
+            <Link className=" relative mr-5 text-2xl">
+              <HiOutlineShoppingBag />
+              <div className="items_count">
+                <span className="text-white">0</span>
+              </div>
+            </Link>
+          </li>
+        </div>
+      </div>
+
+      <div>
+        <Sidebar isSidebarOpen={isSidebarOpen} closeSidebar={toggleSidebar} />
+      </div>
+    </div>
+  );
 }
--- src/common/Sidebar.jsx
+++ src/common/Sidebar.jsx
@@ -1,3 +1,29 @@
-export default function Sidebar() {
-  return <div>Sidebar</div>;
+/* eslint-disable react/prop-types */
+import { FaTimes } from "react-icons/fa";
+
+export default function Sidebar({ isSidebarOpen, closeSidebar }) {
+  return (
+    <div>
+      <div>
+        <div
+          className="fixed top-0 right-0 bg-white shadow-lg h-full transition-transform duration-300 ease-in-out overflow-y-auto "
+          style={{ transform: `translateX(${isSidebarOpen ? "0" : "100%"})` }}
+        >
+          <div className="border-b mb-4">
+            <h1 className="text-3xl p-4">Your cart</h1>
+          </div>
+
+          <div className="p-4">
+            <span className="absolute top-0 right-0 p-4" onClick={closeSidebar}>
+              <FaTimes onClick={closeSidebar} />
+            </span>
+
+            <div className="text-3xl font-bold uppercase">
+              Your cart Has No Product
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
