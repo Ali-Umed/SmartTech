b0e2be8603a782b7921a77693a52adc5e5161255
Ali-Umed <aliumed844@gmail.com>
2024-07-05 18:52:30 +0300

    chore: update dependecy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Header from "./common/Header";
 import Cart from "./pages/Cart";
 import Shop from "./pages/Shop";
-import AboutUs from "./pages/AboutUs";
 import Blog from "./pages/Blog";
 import NotFoundPage from "./pages/NotFoundPage";
 import Footer from "./common/Footer";
@@ -18,7 +17,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<AboutUs />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/*" element={<NotFoundPage />} />
         </Routes>
